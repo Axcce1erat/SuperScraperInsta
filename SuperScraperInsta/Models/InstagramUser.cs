626afa4f8c14fe656cb695a20f83660aef3bc5d0
Axel <axelschwarz@gmx.de>
2020-09-23 22:30:52 +0200

    Add project files.

---
+++ SuperScraperInsta/Models/InstagramUser.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperScraper.Models
+{
+    public class InstagramUser
+    {
+        public string FullName { get; set; }
+        public int FollowerCount { get; set; }
+        public int FollowingCount { get; set; }
+
+        
+        // Display this user in the console       
+        public void Display()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"-------------------------------------");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Full name: {FullName}");
+            Console.WriteLine($"Followers: {FollowerCount}");
+            Console.WriteLine($"Following: {FollowingCount}");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"-------------------------------------");
+        }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
---
+++ SuperScraperInsta/run_debug.bat
@@ -0,0 +1,2 @@
+@ECHO off
+call "bin\Debug\netcoreapp3.1\SuperScraperInsta.exe" %*
